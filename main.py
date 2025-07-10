9d74c71cb30eb06dfa6cc06007ab8e9134253cfd
Adeel Khan <mak000@gmail.com>
2025-07-11 00:43:17 +0500

    Integrates Google Books api via tool call

--- main.py
+++ main.py
@@ -10,19 +10,17 @@ from tools import chatbot_tools, selected_tool
 from langchain_core.messages import HumanMessage, SystemMessage
 
 messages = []
-messages.append(SystemMessage("You are helpful chatbot"))
-
+messages.append(SystemMessage("You are helpful Assistant"))
 llm_with_tools = llm.bind_tools(chatbot_tools)
 
-user_query = input("Please ask me anything?")
+user_query = input("Please ask me anything? ")
 messages.append(HumanMessage(user_query))
 response = llm_with_tools.invoke(messages)
 
 for tool_call in response.tool_calls:
   tool = selected_tool[tool_call["name"].lower()] # finding the tool messages
-  print("tool", tool)
   tool_msg = tool.invoke(tool_call) # invoking the tool messages
   messages.append(tool_msg)  
 
 response = llm.invoke(messages) # invoking llm 
-print("final output", response.content)
+print("Response: \n", response.content)
--- tools.py
+++ tools.py
@@ -1,8 +1,15 @@
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_core.tools import tool
+from langchain_community.tools.google_books import GoogleBooksQueryRun
+from langchain_community.utilities.google_books import GoogleBooksAPIWrapper 
+
 
 # searching via duckduckgo
 duck_search = DuckDuckGoSearchRun()
+
+# google books to search for book and its review
+book_api_tool = GoogleBooksQueryRun(api_wrapper=GoogleBooksAPIWrapper())
+
 
 @tool
 def add(a: int, b: int) -> int: 
@@ -24,8 +31,8 @@ def multiply(a: int, b: int) -> int:
   return a * b
 
 
-selected_tool = {"add": add, "multiply": multiply, "duckduckgo_search": duck_search}
-chatbot_tools = [add, multiply, duck_search]
+selected_tool = {"add": add, "multiply": multiply, "duckduckgo_search": duck_search, "googlebooks": book_api_tool}
+chatbot_tools = [add, multiply, duck_search, book_api_tool]
 
 
 
